efa8045f5aa88913576ae803a26b1d6f4830072d
boui <rocksyseletska2837@gmail.com>
2014-11-23 17:29:20 +0000

    adding bit strings

---
+++ src/main/java/algz/binary/Operations.java
@@ -0,0 +1,32 @@
+package algz.binary;
+
+import com.sun.xml.internal.fastinfoset.util.CharArray;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Operations {
+
+    public String adding(String s1, String s2) {
+        String result = "";
+        int carry=0, d1bit, d2bit;
+        int n1 = s1.length(), n2 = s2.length();
+        for(int i = 0; i < Math.max(n1, n2) ;i++) {
+            d1bit = i< n1 ? s1.charAt(n1-i-1) - '0': 0;
+            d2bit = i< n2 ? s2.charAt(n2-i-1) - '0': 0;
+            result = (char)((d1bit ^ d2bit ^ carry) + '0') + result;
+            carry = d1bit & d2bit | d1bit & carry | d2bit & carry;
+        }
+
+        result = (char)(carry+'0') + result;
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Operations op = new Operations();
+        System.out.println(op.adding("001", "111"));//1000
+        System.out.println(op.adding("000", "1111"));//1111
+        System.out.println(op.adding("101", "111"));//1100
+    }
+}
