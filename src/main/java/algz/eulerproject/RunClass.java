4d06b4291f073c3f3759d1a409f35bcc45c33dbb
boui <rocksyseletska2837@gmail.com>
2014-11-29 22:38:54 +0000

    runner updated for euler project

--- src/main/java/algz/eulerproject/RunClass.java
+++ src/main/java/algz/eulerproject/RunClass.java
@@ -8,7 +8,15 @@ public class RunClass {
     public static void main(String[] array){
 //        System.out.println(new T1().count(3,5,999));
 //        System.out.println(new T2().fib());
-        System.out.println(new T3().largestPrimeFactor(600851475143l));
+//        System.out.println(new T3().largestPrimeFactor(600851475143l));
+//        System.out.println(new T4().findLargestPalidrome());
+//        System.out.println(new T5().findMinimal());
+//        System.out.println(new T6().diff(100));
+//        System.out.println(new T7().findPrimeByIndex(10001));
+//        System.out.println(new T8().getProduct(13));
+//        System.out.println(new T9().findProduct());
+//        System.out.println(new T10().getSumOfPrimes(2000000));
+        System.out.println(new T11(4).getMaxProduct());
 
     }
 }
