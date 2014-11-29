3940ba8b9c1b41163d85bb9ea1ec2656f0b29800
boui <rocksyseletska2837@gmail.com>
2014-11-29 22:37:24 +0000

    adding two binaries updated

--- src/main/java/algz/binary/Operations.java
+++ src/main/java/algz/binary/Operations.java
@@ -9,17 +9,16 @@ import java.util.Map;
 public class Operations {
 
     public String adding(String s1, String s2) {
-        String result = "";
+        StringBuilder result = new StringBuilder();
         int carry=0, d1bit, d2bit;
         int n1 = s1.length(), n2 = s2.length();
         for(int i = 0; i < Math.max(n1, n2) ;i++) {
             d1bit = i< n1 ? s1.charAt(n1-i-1) - '0': 0;
             d2bit = i< n2 ? s2.charAt(n2-i-1) - '0': 0;
-            result = (char)((d1bit ^ d2bit ^ carry) + '0') + result;
+            result.insert(0, (char)((d1bit ^ d2bit ^ carry) + '0'));
             carry = d1bit & d2bit | d1bit & carry | d2bit & carry;
         }
-
-        return carry == 0 ? result : (char)(carry+'0')+result;
+        return carry == 0 ? result.toString() : result.insert(0, (char)(carry+'0')).toString();
     }
 
     public static void main(String[] args) {
